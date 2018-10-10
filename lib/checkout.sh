55c46cec33dfdeb87a442537bd2644f5cb7503a6
Guilherme Paixão <guiferpa@gmail.com>
2018-10-10 16:41:53 -0300

    Add create branch inheriting a HEAD commit from forked branch

--- lib/checkout.sh
+++ lib/checkout.sh
@@ -1,4 +1,16 @@
+# cc - current commit id
+checkout_cfromcommit() {
+  local cc=`git show-ref --head | grep HEAD | awk '{print $1}'`
+  echo $cc > $heads/$1
+  echo "ref: refs/heads/$1" > $head
+  echo "Switched to branch: "$cyan"$1"$white" from $cyan"$cc$white
+  okfn
+}
+
 checkout() {
+  if [ "${2}" == "-b" ]; then
+    checkout_cfromcommit $3
+  fi
   echo "ref: refs/heads/$2" > $head
   echo "Switched to branch: "$cyan"$2"$white
   okfn
