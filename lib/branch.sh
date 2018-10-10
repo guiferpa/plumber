2643e4be0930025c5ec432cdcf3eb1b823dbf2b4
Guilherme Paixão <guiferpa@gmail.com>
2018-10-10 00:27:41 -0300

    Improviment branch command

--- lib/branch.sh
+++ lib/branch.sh
@@ -1,10 +1,15 @@
 branch() {
   cb=`cat .git/HEAD | awk '{print substr($2, 12, length($2))}'`
-  bs=`ls $gitf/refs/heads`
-  echo "Current branch: "$cyan"$cb"$white
+  bs=`ls -1 $gitf/refs/heads | sort`
   if [ ! -z "${bs}" ]; then
-    echo "List of branch:"
-    for b in $bs; do echo " - $b"; done
+    for b in $bs; do
+      h=`cat $gitf/refs/heads/$b`
+      if [ "${b}" == "${cb}" ]; then
+        echo $cyan"$b"$white" -> $h"$white
+      else
+        echo $white"$b"$white" -> $h"$white
+      fi
+    done | column
   fi
   okfn
 }
