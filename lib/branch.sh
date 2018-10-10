7abb675fd5c0d093263615123f7c4c13e60dafd2
Guilherme Paixão <guiferpa@gmail.com>
2018-10-10 19:45:38 -0300

    Improviment the output of branch list

--- lib/branch.sh
+++ lib/branch.sh
@@ -1,21 +1,6 @@
-# cb - current branch
-# bs - branches
-# b - branch
-# tmpl - template
-# c - commit id
+# fmt - format
 branch() {
-  local cb=`cbranchfn`
-  local bs=`ls -1 $heads | sort`
-  if [ ! -z "${bs}" ]; then
-    for b in $bs; do
-      local c=`cat $heads/$b`
-      local tmpl="$b"$white" -> $c"$white
-      if [ "${b}" == "${cb}" ]; then
-        echo $cyan"$tmpl"
-      else
-        echo $white"$tmpl"
-      fi
-    done | column
-  fi
+  local fmt="%(if)%(HEAD)%(then)$cyan* $white%(else)%(end)%(refname:short) -> %(objectname)"
+  git for-each-ref --format="$fmt" refs/heads/ | column
   okfn
 }
