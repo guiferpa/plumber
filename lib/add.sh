f51b20089969d493d7dd82d86227ee09be8aedbd
Guilherme Paixão <guiferpa@gmail.com>
2018-10-11 17:03:20 -0300

    Update add command to verify if file exists

--- lib/add.sh
+++ lib/add.sh
@@ -1,16 +1,16 @@
 # b - blob
 add() {
   for a in $@; do
-    if [[ -f "${a}" ]]; then
+    `cat $a > /dev/null 2> /dev/null`
+    if [[ "${?}" == "0" ]]; then
       local b=`git hash-object -w $a`
       if [ -z "${b}" ]; then
         echo $white"Something wrong when created blob"$white
         failfn 1
       fi
       `git update-index --add --cacheinfo 100644 $b $a`
+      echo $cyan"Blob$white $b as $a added in index"
     fi
-    echo $cyan"Blob$white $b as $a added in index"
   done
-
   okfn
 }
